d7c89226bdd264a0af60d946b793598953c38060
TAAT <20purpleblue10@gmail.com>
2023-01-11 13:54:40 +0900

    Initial Commit

---
+++ progressview/progressviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  progressviewApp.swift
+//  progressview
+//
+//  Created by 董 亜飛 on 2023/01/11.
+//
+
+import SwiftUI
+
+@main
+struct progressviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
